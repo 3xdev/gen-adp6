d1eba029800ddc261ad181df3d97a076c3856242
liux <lg970044@foxmail.com>
2023-04-04 14:05:47 +0800

    chore: pro i18n-remove --locale=zh-CN --write

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,19 +1,7 @@
 import { QuestionCircleOutlined } from '@ant-design/icons';
-import { SelectLang as UmiSelectLang } from '@umijs/max';
+import '@umijs/max';
 import React from 'react';
-
 export type SiderTheme = 'light' | 'dark';
-
-export const SelectLang = () => {
-  return (
-    <UmiSelectLang
-      style={{
-        padding: 4,
-      }}
-    />
-  );
-};
-
 export const Question = () => {
   return (
     <div
